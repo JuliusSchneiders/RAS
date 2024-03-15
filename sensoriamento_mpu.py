0bba6faebd4d196fc87ed9567dfab368d5e54d20
JuliusSchneiders <163591428+JuliusSchneiders@users.noreply.github.com>
2024-03-15 17:19:55 -0300

    Create sensoriamento_mpu.py

---
+++ sensoriamento_mpu.py
@@ -0,0 +1,80 @@
+import machine
+from time import sleep
+import math
+
+from machine import Pin
+
+class accel():
+    def __init__(self, i2c, addr=0x68):
+        self.iic = i2c
+        self.addr = addr
+        self.iic.start()
+        self.iic.writeto(self.addr, bytearray([107, 0]))
+        self.iic.stop()
+
+    def valores_brutos(self):
+        self.iic.start()
+        a = self.iic.readfrom_mem(self.addr, 0x3B, 14)
+        self.iic.stop()
+        return a
+
+    def bytes_toint(self, primeirobyte, segundobyte):
+        if not primeirobyte & 0x80:
+            return primeirobyte << 8 | segundobyte
+        return - (((primeirobyte ^ 255) << 8) | (segundobyte ^ 255) + 1)
+
+    def valores(self):
+        raw_ints = self.valores_brutos()
+        vals = {}
+        vals["AcX"] = self.bytes_toint(raw_ints[0], raw_ints[1])
+        vals["AcY"] = self.bytes_toint(raw_ints[2], raw_ints[3])
+        vals["AcZ"] = self.bytes_toint(raw_ints[4], raw_ints[5])
+        vals["Tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) / 340.00 + 36.53
+        vals["GyX"] = self.bytes_toint(raw_ints[8], raw_ints[9])
+        vals["GyY"] = self.bytes_toint(raw_ints[10], raw_ints[11])
+        vals["GyZ"] = self.bytes_toint(raw_ints[12], raw_ints[13])
+        return vals  # returned in range of Int16
+        # -32768 to 32767
+
+    def ang_x(self):
+        vals = self.valores()
+        angle_x = math.atan(vals["AcX"] / math.sqrt(vals["AcY"]**2 + vals["AcZ"]**2)) * 180 / math.pi
+        return ang_x
+    def ang_y(self):
+        vals = self.valores()
+        angle_y = math.atan(vals["AcY"] / math.sqrt(vals['AcX']**2 + vals['AcZ']**2)) * 180 / math.pi
+        return ang_y
+
+    def valores_temp(self):
+        raw_ints = self.valores_brutos()
+        vals = {}
+        vals["Tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) / 340.00 + 36.53
+        return vals
+
+    def calculate_temp(self):
+        vals = self.valores_temp()
+        temp = vals['Tmp']
+        return temp
+
+# Inicializa o barramento I2C
+i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21))
+
+# Inicializa o objeto accel
+sensor = accel(i2c)
+
+led = Pin(2, Pin.OUT)
+
+# Calcula e imprime o ângulo do eixo X e a temperatura
+while True:
+    angulo_x = sensor.ang_x()
+    angulo_y = sensor.ang_y()
+    temp = sensor.calculate_temp()
+    print(f'\nângulo x: {angulo_x}  \nângulo y: {angulo_y}  \ntemperatura: {temp}\n =-=-=-=-=-=-=-=-=-=-=-=-=-=-=)')
+    sleep(0.1)
+    
+    if angulo_y > 30 or angulo_y < -30:
+        led.value(1)
+        sleep(0.2)
+        led.value(0)
+        sleep(0.1)
+        
